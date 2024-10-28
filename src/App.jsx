ebc1765006655a8533a4008d94259d344170e452
htilssu <tolashuu@gmail.com>
2024-10-28 10:42:38 +0700

    add pusher

--- src/App.jsx
+++ src/App.jsx
@@ -6,13 +6,19 @@ import {ReactQueryDevtools} from '@tanstack/react-query-devtools';
 import {QueryClientProvider} from '@tanstack/react-query';
 import {queryClient} from './modules/cache.js';
 import {AuthProvider} from './modules/hooks/useAuth.jsx';
-import {Suspense} from 'react';
+import {Suspense, useEffect} from 'react';
 import LoadingPageSkeleton from './components/LoadingPageSkeleton.jsx';
 import 'react-toastify/dist/ReactToastify.css';
+import pusher from './services/pusher.js';
 
 
 function App() {
   const mode = import.meta.env.MODE;
+
+  useEffect(() => {
+    pusher.subscribe('')
+  }, []);
+
   return (
       <>
         <Suspense fallback={<LoadingPageSkeleton/>}>
