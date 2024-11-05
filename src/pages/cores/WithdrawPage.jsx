7d50814a09f53848ce8a79045fafe8bd1c8d5f75
htilssu <tolashuu@gmail.com>
2024-11-05 12:40:34 +0700

    d

--- src/pages/cores/WithdrawPage.jsx
+++ src/pages/cores/WithdrawPage.jsx
@@ -1,158 +1,146 @@
 import {FaUpload} from 'react-icons/fa';
 import {useState} from 'react';
-import {CiBank} from 'react-icons/ci';
 import {WalletSection} from '../../components/account/WalletSection.jsx';
+import {Button, NumberInput} from '@mantine/core';
+import {useQuery} from '@tanstack/react-query';
+import {getMyWallet} from '../../modules/wallet/wallet.js';
+import {getRevealFormat} from '../../util/number.util.js';
+import {wGet} from '../../util/request.util.js';
 
 const WithdrawPage = () => {
   const [amount, setAmount] = useState('');
-  const [displayAmount, setDisplayAmount] = useState('');
-  const [error, setError] = useState(false);
+  const [error, setError] = useState('');
+  const [suggestAmount, setSuggestAmount] = useState(0);
+  const [selectedCard, setSelectedCard] = useState();
 
-  const handleAmountChange = (e) => {
-    const value = e.target.value;
-    const numericValue = value.replace(/[^0-9]/g, '');
-    setAmount(numericValue);
-    setDisplayAmount(numericValue);
-    if (parseInt(numericValue, 10) >= 100000 || numericValue === '') {
-      setError(false);
-    }
-  };
+  const {data: wallet} = useQuery({
+    queryKey: ['wallet'], queryFn: async () => getMyWallet(),
+  });
+  const {data: bankList} = useQuery({
+    queryKey: ['bank-list'], queryFn: async () => await wGet('/v1/banks'), staleTime: 1000 * 60 * 60,
+  });
+  const {
+    data: cardList,
+  } = useQuery({
+    queryKey: ['card-list'], queryFn: async () => await wGet('/v1/card'), staleTime: 1000 * 60 * 10,
+  });
 
-  const handleAmountBlur = () => {
-    if (amount) {
-      const formattedAmount = new Intl.NumberFormat('vi-VN', {
-        style: 'currency',
-        currency: 'VND',
-      }).format(amount);
-      setDisplayAmount(formattedAmount);
-    }
-  };
+  function handleSelectCard(card) {
+    setSelectedCard(card.id);
+  }
 
   const suggestedAmounts = [100000, 200000, 300000, 400000, 500000, 1000000];
 
-  const handleContinue = (e) => {
-    e.preventDefault(); // Ngăn chặn hành động mặc định của form
-    if (!amount || parseInt(amount, 10) < 100000) {
-      setError(true);
-      return;
+  const handleAmountChange = (e) => {
+    setError('');
+    setSuggestAmount(0);
+    if (parseInt(e) > parseInt(wallet?.balance)) {
+      setError('Số tiền rút không được lớn hơn số dư trong ví.');
     }
-    console.log('Continue button clicked with amount:', amount);
+    setAmount(e);
   };
 
-  return (
-      <div className="flex flex-col md:flex-row justify-center">
-        <div className="w-full md:w-2/3 lg:w-1/2 bg-white shadow-md rounded-lg overflow-hidden mb-9">
-          <div className="flex items-center p-4 bg-gradient-to-r from-cyan-500 to-blue-500 text-white rounded-t-lg">
-            <FaUpload size={25}/>
-            <h2 className="text-2xl font-bold ml-3">Rút tiền</h2>
-          </div>
-          <form className="mt-5 px-4 md:px-8 lg:px-20" onSubmit={handleContinue}>
-            <div className="mb-6 flex justify-start">
-              <h1 className="text-xl md:text-2xl lg:text-4xl text-gray-500 font-light">
-                Thông tin rút tiền
-              </h1>
-            </div>
-            <div className="mb-2 flex flex-col">
-              <div className="flex items-center mb-4">
-                <label
-                    className="mr-4 text-gray-700 text-sm font-bold mb-2 "
-                    htmlFor="amount"
-                >
-                  Số Tiền
-                </label>
-                <input
-                    type="text"
-                    id="amount"
-                    placeholder="Số tiền tối thiểu phải lớn hơn 100,000 VND"
-                    value={displayAmount}
-                    onChange={handleAmountChange}
-                    onBlur={handleAmountBlur}
-                    className={`placeholder:text-sm flex-grow text-2xl md:text-lg w-full px-3 py-2 text-red-700 font-sans ring-1 hover:ring-emerald-500 rounded-lg focus:outline-none focus:shadow-outline ${error
-                                                                                                                                                                                                             ? 'ring-red-500'
-                                                                                                                                                                                                             : 'ring-blue-500'}`}
-                />
-              </div>
-              {error && (
-                  <p className="text-red-600 text-md ml-2 md:ml-4 mb-4">
-                    Vui lòng nhập số tiền hợp lệ (tối thiểu 100,000 VND)
-                  </p>
-              )}
-              <div className="grid grid-cols-2 gap-2 md:gap-4 justify-center md:grid-cols-3">
-                {suggestedAmounts.map((val) => (
-                    <button
-                        key={val}
-                        type="button"
-                        onClick={() => {
-                          const formattedAmount = new Intl.NumberFormat('vi-VN', {
-                            style: 'currency',
-                            currency: 'VND',
-                          }).format(val);
-                          setAmount(val.toString());
-                          setDisplayAmount(formattedAmount);
-                          setError(false);
-                        }}
-                        className="h-10 bg-gradient-to-r from-cyan-500 to-blue-500 text-white py-2 px-1
-                   rounded-lg hover:from-cyan-600 hover:to-blue-600 hover:bg-gradient-to-r focus:outline-none focus:shadow-outline transition duration-300"
-                    >
-                      {val.toLocaleString()}
-                    </button>
-                ))}
-              </div>
-            </div>
+  function handleSubmitWithdraw() {
+    setError('');
+    if (parseInt(amount) > parseInt(wallet?.balance)) {
+      setError('Số tiền rút không được lớn hơn số dư trong ví.');
+    }
+    if (!selectedCard) {
+      setError('Vui lòng chọn thẻ liên kết.');
+    }
 
-            <div className="mt-5 border-t-[1px] border-gray-600">
-              <label className="block text-gray-700 text-sm font-bold mb-3 mt-5">
-                CHỌN PHƯƠNG THỨC NẠP
-              </label>
-              <div
-                  className="flex border-[1px] h-[100px] w-full md:w-1/2 rounded-xl items-center justify-center bg-amber-100">
-                <div className="text-5xl text-amber-500 mr-5">
-                  <CiBank/>
-                </div>
-                <div>
-                  <p className="text-black font-medium">
-                    Rút tiền về tài khoản ngân hàng
-                  </p>
-                  <a
-                      href="#"
-                      className="decoration-0 text-sky-500 hover:text-amber-500"
-                  >
-                    Xem biểu phí rút
-                  </a>
-                </div>
-              </div>
-            </div>
-            <div className="mt-6 p-4 bg-yellow-100 border-l-4 border-yellow-500 text-yellow-700 rounded-2xl">
-              <p className="font-bold">Thông báo:</p>
-              <div className="flex">
-                <p>
-                  THT sẽ thu 1% trên tổng số tiền rút nếu nguồn tiền nhận được từ
-                  giao dịch nạp. Giấy ủy quyền sau khi xác nhận, vui lòng gửi về
-                  email THT@htilssu.com. Mẫu giấy ủy quyền{' '}
-                  <a
-                      href="#"
-                      className="decoration-0 text-sky-500 hover:text-amber-500"
-                  >
-                    tại đây.
-                  </a>
-                </p>
-              </div>
-            </div>
-            <div className="flex justify-end mt-5 mb-6">
-              <button
-                  type="submit"
-                  className="px-3 py-2 text-sm bg-gradient-to-r from-cyan-500 to-blue-500 text-white rounded-lg hover:from-cyan-600 hover:to-blue-600 hover:bg-gradient-to-r focus:outline-none focus:shadow-outline transition duration-300"
-              >
-                Tiếp tục
-              </button>
-            </div>
-          </form>
+    //TODO: Implement withdraw function
+  }
+
+  return (<div className="flex flex-col md:flex-row justify-center">
+    <div className="w-full md:w-2/3 lg:w-1/2 bg-white shadow-md rounded-lg overflow-hidden mb-9">
+      <div className="flex items-center p-4 bg-gradient-to-r from-cyan-500 to-blue-500 text-white rounded-t-lg">
+        <FaUpload size={25}/>
+        <h2 className="text-2xl font-bold ml-3">Rút tiền</h2>
+      </div>
+      <div className={'p-4'}>
+        <div className="mb-6 flex justify-start">
+          <h1 className="text-xl md:text-2xl lg:text-4xl text-gray-500 font-light">
+            Thông tin rút tiền
+          </h1>
         </div>
-        <div className="order-first md:order-last w-full md:w-1/3 lg:w-1/4 mb-6 md:mb-0 md:ml-9">
-          <WalletSection/>
+        <div className="mb-2 flex flex-col">
+          <div className="mb-4">
+            <label
+                className="mr-4 text-gray-700 text-sm font-bold mb-2 "
+                htmlFor="amount"
+            >
+              Số Tiền
+            </label>
+            <NumberInput
+                placeholder="Số tiền cần rút"
+                value={amount}
+                size="md"
+                min={10000}
+                error={error}
+                thousandSeparator={','}
+                suffix={''}
+                rightSection={<div className={'mr-10'}>VNĐ</div>}
+                allowNegative={false}
+                allowDecimal={false}
+                onChange={handleAmountChange}
+                hideControls
+            />
+          </div>
+          <p className="text-gray-700 text-md font-semibold mb-2">Số tiền đề xuất:</p>
+          <div className="grid grid-cols-2 gap-2 md:gap-4 justify-center md:grid-cols-3">
+            {suggestedAmounts.map((val) => (<button
+                key={val}
+                type="button"
+                onClick={() => {
+                  setAmount(val.toString());
+                  setSuggestAmount(val);
+                  setError(false);
+                }}
+                className={`h-10 ${val === suggestAmount
+                                   ? '!bg-green-400 text-white'
+                                   : 'bg-white'} border text-gray-700 py-2 px-4 rounded-lg hover:bg-gray-100 focus:outline-none focus:shadow-outline transition duration-300`}
+            >
+              {val.toLocaleString()}
+            </button>))}
+          </div>
+          <div>
+            <label className="block text-gray-700 text-sm font-bold mt-3">
+              CHỌN THẺ LIÊN KẾT
+            </label>
+          </div>
+          <div className={'w-full grid grid-cols-2 gap-3 mt-2'}>
+            {Array.isArray(cardList) && cardList?.map((card) => {
+              const bank = bankList?.find(b => b.id == card.atmId);
+              return ((<div key={card.id}
+                            className={`${card.id === selectedCard &&
+                            '!bg-green-100 border-green-500'} w-full flex items-center rounded hover:border-green-500 hover:bg-gray-100 transition-colors px-2 py-3 border`}
+                            onClick={() => handleSelectCard(card)}
+              >
+                <img src={bank?.logo} className={'w-1/3'} alt="bank logo"/>
+                <div className={'text-center w-full'}>{getRevealFormat(card.cardNumber.toString())}</div>
+              </div>));
+            })}
+          </div>
+          <div className="flex justify-end mt-5">
+            <Button
+                onClick={handleSubmitWithdraw}
+                color="green"
+                type="submit"
+                size={'md'}
+                className="w-full mt-3"
+            >
+              Tiếp tục
+            </Button>
+          </div>
         </div>
       </div>
-  );
+
+    </div>
+    <div className="order-first md:order-last w-full md:w-1/3 lg:w-1/4 mb-6 md:mb-0 md:ml-9">
+      <WalletSection/>
+    </div>
+  </div>);
 };
 
 export default WithdrawPage;
