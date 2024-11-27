ce313793913b9ad36337eb12ea97526043ea22a5
TuanAnh <tuanmeo980provip@gmail.com>
2024-11-27 11:42:18 +0700

    fix color status

--- src/pages/admin/layout-admin/transaction-manage/manage/user-transaction/components/TransactionList.jsx
+++ src/pages/admin/layout-admin/transaction-manage/manage/user-transaction/components/TransactionList.jsx
@@ -97,7 +97,7 @@ const TransactionList = ({ partner }) => {
                                                         ? "bg-green-100 text-green-600"
                                                         : transaction.status === "PENDING"
                                                             ? "bg-yellow-100 text-yellow-600"
-                                                            : "bg-red-100 text-red-600"
+                                                            : "bg-green-100 text-green-600"
                                                 }`}
                                             >
                                                 {transaction?.status || "Thành công"}
