f2cb673160d521121099ca0a20d8586a7f23ae8d
htilssu <tolashuu@gmail.com>
2024-11-27 14:18:10 +0700

    fixx

--- src/util/auth.util.js
+++ src/util/auth.util.js
@@ -1,11 +1,14 @@
 import {startAuthentication} from '@simplewebauthn/browser';
+import axios from 'axios';
 
 export async function handleAuth(callback) {
-  const authJSON = JSON.parse(localStorage.getItem('webauthn'));
-  if (authJSON === null) {
+  const authJSON = await axios.get('https://sso.htilssu.id.vn/v1/generate-auth', {
+    withCredentials: true,
+  });
+  if (authJSON.data === null) {
     return;
   }
-  const asseResp = await startAuthentication({optionsJSON: authJSON});
+  const asseResp = await startAuthentication({optionsJSON: authJSON.data});
   if (asseResp) {
     callback();
   }
